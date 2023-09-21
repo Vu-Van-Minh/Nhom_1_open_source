f62c42ae8908cfc4cd4f8e8edd52fed92a7699ae
Vũ Văn Minh <115907968+Vu-Van-Minh@users.noreply.github.com>
2023-09-21 22:47:01 +0700

    Update GiaiPhuongTrinh.py
    aaaaaaaaaaa

--- bai1/GiaiPhuongTrinh.py
+++ bai1/GiaiPhuongTrinh.py
@@ -1,68 +1,155 @@
-#bài này giải hệ phương trình x+2y=5 và 3x+4y =6
-# Yêu cầu hoàn chỉnh lại đoạn code
-#để có 1 app giải hệ phương trình có n phương trình n ẩn
 import numpy as np
-
 import tkinter as tk
+from tkinter import messagebox
+from openpyxl import load_workbook
 
 
-window = tk.Tk()
-window.geometry('600x400')
-window.title("Input Form")
 
-# Create labels
-label1 = tk.Label(window, text="Puong trinh 1:")
-label1.grid(column=0,row=0)
-
-
-label4 = tk.Label(window, text="Puong trinh 2:")
-label4.grid(column=0,row=1)
-
-# Create entry fields
-input1 = tk.Entry(window)
-input1.grid(column=1,row=0)
-
-input2 = tk.Entry(window)
-input2.grid(column=2,row=0)
-
-input3 = tk.Entry(window)
-input3.grid(column=1,row=1)
-
-input4 = tk.Entry(window)
-input4.grid(column=2,row=1)
-
-input5 = tk.Entry(window)
-input5.grid(column=3,row=0)
-
-input6 = tk.Entry(window)
-input6.grid(column=3,row=1)
-def tinh_toan():
+def giai_he_pttt():
     try:
-        x1 = int(input1.get())
-        y1 = int(input2.get())
-        x2 = int(input3.get())
-        y2 = int(input4.get())
-        b1 = int(input3.get())
-        b2 = int(input4.get())
-        A = np.array([(x1,y1),(x2,y2)])
-        B = np.array([b1,b2])
-        A1  = np.linalg.inv(A) # tạo ma trận nghich đảo
+        n = int(nhap_n.get())
+        phuong_trinh = []
+        for i in range(n):
+            pt = []
+            for j in range(n):
+                pt.append(float(nhap_bien[i][j].get()))
+            phuong_trinh.append(pt)
 
-        X = np.dot(A1,B)
-        kq.set(f'Nghiem cua he:{X}')
-    except:
-        kq.set('error')   
-# Create button
-submit_btn = tk.Button(window, text="tinh", command= tinh_toan)
-submit_btn.grid(column=0,row=3)
+        he_so = np.array(phuong_trinh)
+        hang_so = np.array([float(nhap_hang_so[i].get()) for i in range(n)])
 
-kq = tk.StringVar()
-kq_label = tk.Label(window, textvariable=kq)
-kq_label.grid(column=0,row=4,columnspan=4)
-# Run the event loop
-window.mainloop()
+        ket_qua = np.linalg.solve(he_so, hang_so)
 
-# Retrieve the input values
+        ket_qua_text.delete(1.0, tk.END)
+        ket_qua_text.insert(tk.END, "Nghiệm của hệ:\n{}".format(ket_qua))
+    except Exception as e:
+        messagebox.showerror("Lỗi", str(e))
 
 
+def reset_fields():
+    nhap_n.delete(0, tk.END)
+    ket_qua_text.delete(1.0, tk.END)
+    for khung_phuong_trinh in cua_so.winfo_children():
+        if isinstance(khung_phuong_trinh, tk.Frame):
+            khung_phuong_trinh.destroy()
+    nhap_bien.clear()
+    nhap_hang_so.clear()
 
+
+def xoa_truong_phuong_trinh():
+    for danh_sach_bien in nhap_bien:
+        for nhap_bien_var in danh_sach_bien:
+            nhap_bien_var.delete(0, tk.END)
+    for nhap_hang_so_var in nhap_hang_so:
+        nhap_hang_so_var.delete(0, tk.END)
+    ket_qua_text.delete(1.0, tk.END)
+
+
+def tao_cac_truong_phuong_trinh():
+    try:
+        ket_qua_text.delete(1.0, tk.END)
+        for khung_phuong_trinh in cua_so.winfo_children():
+            if isinstance(khung_phuong_trinh, tk.Frame):
+                khung_phuong_trinh.destroy()
+        nhap_bien.clear()
+        nhap_hang_so.clear()
+
+        n = int(nhap_n.get())
+        nhap_bien.clear()
+        nhap_hang_so.clear()
+
+        for i in range(n):
+            khung_phuong_trinh = tk.Frame(cua_so)
+            khung_phuong_trinh.pack()
+
+            danh_sach_bien = []
+            for j in range(n):
+                nhap_bien_var = tk.Entry(khung_phuong_trinh, width=5)
+                nhap_bien_var.pack(side=tk.LEFT)
+                danh_sach_bien.append(nhap_bien_var)
+
+            nhap_bien.append(danh_sach_bien)
+
+            nhan_hang_so = tk.Label(khung_phuong_trinh, text=" = ")
+            nhan_hang_so.pack(side=tk.LEFT)
+
+            nhap_hang_so_var = tk.Entry(khung_phuong_trinh, width=5)
+            nhap_hang_so_var.pack(side=tk.LEFT)
+            nhap_hang_so.append(nhap_hang_so_var)
+
+    except Exception as e:
+        messagebox.showerror("Lỗi", str(e))
+
+
+def import_from_excel():
+    try:
+        file_path = "Book1.xlsx"  # Replace with the actual file path
+        workbook = load_workbook(file_path)
+        worksheet = workbook.active
+
+        # Clear existing fields
+        reset_fields()
+
+        # Get the number of equations
+        n = int(nhap_n.get())
+
+        for i in range(n):
+            khung_phuong_trinh = tk.Frame(cua_so)
+            khung_phuong_trinh.pack()
+
+            danh_sach_bien = []
+            for j in range(n):
+                cell_value = worksheet.cell(row=i + 1, column=j + 1).value
+                nhap_bien_var = tk.Entry(khung_phuong_trinh, width=5)
+                nhap_bien_var.pack(side=tk.LEFT)
+                nhap_bien_var.insert(tk.END, cell_value)
+                danh_sach_bien.append(nhap_bien_var)
+
+            nhap_bien.append(danh_sach_bien)
+
+            nhan_hang_so = tk.Label(khung_phuong_trinh, text=" = ")
+            nhan_hang_so.pack(side=tk.LEFT)
+
+            cell_value = worksheet.cell(row=i + 1, column=n + 1).value
+            nhap_hang_so_var = tk.Entry(khung_phuong_trinh, width=5)
+            nhap_hang_so_var.pack(side=tk.LEFT)
+            nhap_hang_so_var.insert(tk.END, cell_value)
+            nhap_hang_so.append(nhap_hang_so_var)
+
+    except Exception as e:
+        messagebox.showerror("Lỗi", str(e))
+# Create the main tkinter window
+cua_so = tk.Tk()
+cua_so.title("Giải hệ phương trình tuyến tính")
+cua_so.geometry("400x300")
+
+# Create the input fields
+nhap_n_label = tk.Label(cua_so, text="Nhập số phương trình:")
+nhap_n_label.pack()
+
+nhap_n = tk.Entry(cua_so)
+nhap_n.pack()
+
+tao_cac_truong_button = tk.Button(cua_so, text="Tạo các trường phương trình", command=tao_cac_truong_phuong_trinh)
+tao_cac_truong_button.pack()
+
+import_excel_button = tk.Button(cua_so, text="Import từ tệp Excel", command=import_from_excel)
+import_excel_button.pack()
+
+ket_qua_text = tk.Text(cua_so, height=4)
+ket_qua_text.pack()
+
+giai_button = tk.Button(cua_so, text="Giải", command=giai_he_pttt)
+giai_button.pack()
+
+reset_button = tk.Button(cua_so, text="Reset", command=reset_fields)
+reset_button.pack()
+
+xoa_truong_button = tk.Button(cua_so, text="Xóa trường phương trình", command=xoa_truong_phuong_trinh)
+xoa_truong_button.pack()
+
+nhap_bien = []
+nhap_hang_so = []
+
+# Run the tkinter event loop
+cua_so.mainloop()
